e021b1ecf1c70465811a765bbb46c6b8b287c420
nikolasekiw <s333975@oslomet.no>
2020-09-02 09:50:57 +0200

    test nikola

--- WebapplikasjonerOppgave1/Startup.cs
+++ WebapplikasjonerOppgave1/Startup.cs
@@ -13,6 +13,8 @@ using Microsoft.Extensions.Hosting;
 
 namespace WebapplikasjonerOppgave1
 {
+
+    //nikola
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
