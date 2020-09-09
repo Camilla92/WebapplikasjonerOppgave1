464864e2248d08989b488fcbd18c8996ed8b0a9f
martineea <martine.holmberg@gmail.com>
2020-09-09 14:59:18 +0200

    PRøvd å legge til ny Model - Stasjon

--- WebapplikasjonerOppgave1/Controllers/BestillingController.cs
+++ WebapplikasjonerOppgave1/Controllers/BestillingController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using WebapplikasjonerOppgave1.Models;
 using Microsoft.AspNetCore.Mvc;
 using WebapplikasjonerOppgave1.DAL;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Stasjon = WebapplikasjonerOppgave1.Models.Stasjon;
 
 namespace WebapplikasjonerOppgave1.Controllers
 {
@@ -11,27 +14,59 @@ namespace WebapplikasjonerOppgave1.Controllers
     public class BestillingController : ControllerBase
     {
         private readonly NorwayContext _db;
-        
+
         public BestillingController(NorwayContext db)
         {
             _db = db;
         }
 
         //[HttpPost]
-        /*public List<BussBestilling> HentAlleStasjoner()
+        /*public async Task<List<Stasjon>> HentAlleStasjoner()
         {
+            try
+            {
+                List<Stasjon> alleStasjoner = await _db.Stasjoner.Select(k => new Stasjon
+                {
+                    StasjonsNavn = k.StasjonsNavn
+                }).ToListAsync();
+                return alleStasjoner;
+            }
+            catch
+            {
+                return null;
+            }*/
+
+
+        public List<Stasjon> HentAlleStasjoner()
+        {
+            try
+            {
+                List<Stasjon> alleStasjoner = _db.Stasjoner.ToList();
+                return alleStasjoner;
+            }
+            catch
+            {
+                return null;
+            }
+
+
+
+
+
+
+            /*
             List<Stasjon> alleStasjoner = _db.Stasjoner.ToList();
             List<BussBestilling> alleBestillinger = new List<BussBestilling>();
 
 
             var enBestilling = new BussBestilling
             {
-                EndeStasjon = e;
+                EndeStasjon = "Oslo";
              }
 
 
 
-            return alleBestillinger;
-        }*/
+            return alleBestillinger;*/
+        }
     }
 }
---
+++ WebapplikasjonerOppgave1/Models/Stasjon.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace WebapplikasjonerOppgave1.Models
+{
+    public class Stasjon
+    {
+        public int Id { get; set; }
+        public string StasjonsNavn { get; set; }
+    }
+}
