91d428b739f3403ec85c0a43d130bf0624b2120e
martineea <martine.holmberg@gmail.com>
2020-09-16 11:02:09 +0200

    * index.js: * validering.js:
    * BussBestilling.cs: Laget validering på js og model

--- WebapplikasjonerOppgave1/Models/BussBestilling.cs
+++ WebapplikasjonerOppgave1/Models/BussBestilling.cs
@@ -1,14 +1,21 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace WebapplikasjonerOppgave1.Models
 {
 
     public class BussBestilling
     {
         public int Id { get; set; }
+        [RegularExpression(@"^[a-zA-ZæøåÆØÅ. \-]{2,20}$")]
         public string Fornavn { get; set; }
+        [RegularExpression(@"^[a-zA-ZæøåÆØÅ. \-]{2,20}$")]
         public string Etternavn { get; set; }
+        [RegularExpression(@"^[0-9]{8}$")]
         public string Telefonnummer { get; set; }
+        [RegularExpression(@"^[0-9]{2}$")]
         public int AntallBarn { get; set; }
+        [RegularExpression(@"^[0-9]{2}$")]
         public int AntallVoksne { get; set; }
         public double TotalPris { get; set; }
         public DateTime Tid { get; set; }
