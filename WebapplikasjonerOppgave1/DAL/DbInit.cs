e46c7667fc93c3fef994fdd881df43d2df5eff90
synneangell <s331153@oslomet.no>
2020-09-16 11:10:20 +0200

    Laget turer i DBinit

--- WebapplikasjonerOppgave1/DAL/DbInit.cs
+++ WebapplikasjonerOppgave1/DAL/DbInit.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using WebapplikasjonerOppgave1.DAL;
+using WebapplikasjonerOppgave1.Models;
 
 namespace WebapplikasjonerOppgave1.Models
 {
@@ -17,24 +18,9 @@ namespace WebapplikasjonerOppgave1.Models
                 context.Database.EnsureDeleted();
                 context.Database.EnsureCreated();
 
-                /*DateTime date1 = new DateTime (2008, 3, 1, 7, 0, 0 );
-                DateTime date2 = new DateTime(2008, 3, 2, 9, 0, 0);
 
 
-                var tur1 = new Tur { StartStasjon = "Oslo", EndeStasjon = "Bergen", Tid = date1};
-                var tur2 = new Tur { StartStasjon = "Tromsø", EndeStasjon = "Stavanger", Tid = date2 };
-
-                var kunde1 = new Kunde { Navn = "Ole Olsen", Telefonnummer = "12345678" };
-
-
-                context.Turer.Add(tur1);
-                context.Turer.Add(tur2);
-                context.Kunder.Add(kunde1);
-                */
-
-
-
-
+                /*---------OPPRETTER STASJONER--------*/
                 var stasjon1 = new Stasjon { StasjonsNavn = "Oslo" };
                 var stasjon2 = new Stasjon { StasjonsNavn = "Bergen" };
                 var stasjon3 = new Stasjon { StasjonsNavn = "Trondheim" };
@@ -46,10 +32,39 @@ namespace WebapplikasjonerOppgave1.Models
                 context.Stasjoner.Add(stasjon4);
 
 
+                /*---------OPPRETTER DATOER--------*/
+                DateTime date1 = new DateTime(2020, 3, 1, 7, 0, 0);
+                DateTime date2 = new DateTime(2020, 4, 2, 8, 2, 2);
+
+
+                /*---------OPPRETTER TURER--------*/
+                var tur1 = new Tur { StartStasjon = stasjon1, EndeStasjon = stasjon2, Tid = date1, BarnePris = 50, VoksenPris = 100};
+                var tur2 = new Tur { StartStasjon = stasjon1, EndeStasjon = stasjon3, Tid = date2, BarnePris = 50, VoksenPris = 200 };
+                var tur3 = new Tur { StartStasjon = stasjon1, EndeStasjon = stasjon4, Tid = date1, BarnePris = 50, VoksenPris = 100 };
+                var tur4 = new Tur { StartStasjon = stasjon2, EndeStasjon = stasjon1, Tid = date2, BarnePris = 50, VoksenPris = 200 };
+                var tur5 = new Tur { StartStasjon = stasjon2, EndeStasjon = stasjon3, Tid = date1, BarnePris = 50, VoksenPris = 100 };
+                var tur6 = new Tur { StartStasjon = stasjon2, EndeStasjon = stasjon4, Tid = date2, BarnePris = 50, VoksenPris = 200 };
+                var tur7 = new Tur { StartStasjon = stasjon3, EndeStasjon = stasjon1, Tid = date1, BarnePris = 50, VoksenPris = 100 };
+                var tur8 = new Tur { StartStasjon = stasjon3, EndeStasjon = stasjon2, Tid = date2, BarnePris = 50, VoksenPris = 200 };
+                var tur9 = new Tur { StartStasjon = stasjon3, EndeStasjon = stasjon4, Tid = date1, BarnePris = 50, VoksenPris = 100 };
+                var tur10 = new Tur { StartStasjon = stasjon4, EndeStasjon = stasjon1, Tid = date2, BarnePris = 50, VoksenPris = 200 };
+                var tur11 = new Tur { StartStasjon = stasjon4, EndeStasjon = stasjon2, Tid = date1, BarnePris = 50, VoksenPris = 100 };
+                var tur12 = new Tur { StartStasjon = stasjon4, EndeStasjon = stasjon3, Tid = date2, BarnePris = 50, VoksenPris = 200 };
+
+                context.Turer.Add(tur1);
+                context.Turer.Add(tur2);
+                context.Turer.Add(tur3);
+                context.Turer.Add(tur4);
+                context.Turer.Add(tur5);
+                context.Turer.Add(tur6);
+                context.Turer.Add(tur7);
+                context.Turer.Add(tur8);
+                context.Turer.Add(tur9);
+                context.Turer.Add(tur10);
+                context.Turer.Add(tur11);
+                context.Turer.Add(tur12);
 
                 context.SaveChanges();
-
-
 
             }
         }
