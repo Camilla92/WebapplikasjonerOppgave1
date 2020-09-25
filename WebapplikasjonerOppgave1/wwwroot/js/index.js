a2ae56ad384771f112c662396ecfcfe6304dce58
martineea <martine.holmberg@gmail.com>
2020-09-25 11:47:35 +0200

    Fikset bug i css

--- WebapplikasjonerOppgave1/wwwroot/js/index.js
+++ WebapplikasjonerOppgave1/wwwroot/js/index.js
@@ -167,25 +167,30 @@ function genererPopUP() {
 
 
 function lagMinEgenPopUp() {
-    //var modal = document.getElementById("myModal");
+    var modal = document.getElementById("myModal");
 
-    // Get the button that opens the modal*/
+    // Get the button that opens the modal
+
     var btn = document.getElementById("reg");
 
     // Get the <span> element that closes the modal
+
     var span = document.getElementsByClassName("close")[0];
 
     // When the user clicks on the button, open the modal
+
     btn.onclick = function () {
         modal.style.display = "block";
     }
 
     // When the user clicks on <span> (x), close the modal
+
     span.onclick = function () {
         modal.style.display = "none";
     }
 
-    // Når man trykker på avslutt så blir popupen borte.*/
+    // Når man trykker på avslutt så blir popupen borte
+
     avslutt.onclick = function () {
         modal.style.display = "none";
     }
