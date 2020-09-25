b4693f55a136dd91b6e3f9ae2992f15620c70281
Camilla92 <44602961+Camilla92@users.noreply.github.com>
2020-09-25 10:06:52 +0200

    Lagt til avslutt knapp

--- WebapplikasjonerOppgave1/wwwroot/js/index.js
+++ WebapplikasjonerOppgave1/wwwroot/js/index.js
@@ -174,12 +174,11 @@ function lagMinEgenPopUp() {
         modal.style.display = "none";
     }
 
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function (event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
+    // Når man trykker på avslutt så blir popupen borte.*/
+    avslutt.onclick = function () {
+        modal.style.display = "none";
     }
+
 
 }
 
