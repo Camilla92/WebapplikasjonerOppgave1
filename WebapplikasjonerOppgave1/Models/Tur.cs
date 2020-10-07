d450739e7f76c8c5ca93e2e9a03511c19cc3b405
synneangell <s331153@oslomet.no>
2020-10-07 11:41:18 +0200

    Opprettet brukere i DAL

--- WebapplikasjonerOppgave1/Models/Tur.cs
+++ WebapplikasjonerOppgave1/Models/Tur.cs
@@ -9,11 +9,23 @@ namespace WebapplikasjonerOppgave1.Models
     {
        
         public int TurId { get; set; }
+
+        [RegularExpression(@"^[a-zA-ZæøåÆØÅ. \-]{2,20}$")]
         public string StartStasjon { get; set; }
+
+        [RegularExpression(@"^[a-zA-ZæøåÆØÅ. \-]{2,20}$")]
         public string EndeStasjon { get; set; }
+
+        [RegularExpression(@"^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$")]
         public string Dato { get; set; }
+
+
         public string Tid { get; set; }
+
+        
         public double BarnePris { get; set; }
+
+
         public double VoksenPris { get; set; }
     }
 }
