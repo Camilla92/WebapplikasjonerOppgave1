18b694be564aaa680ea0fbbb695621d6f662d200
Camilla92 <44602961+Camilla92@users.noreply.github.com>
2020-10-19 11:01:26 +0200

    Fikset sånn at ikke startstasjon og endestasjon kan være like

--- WebapplikasjonerOppgave1/wwwroot/js/opprettTur.js
+++ WebapplikasjonerOppgave1/wwwroot/js/opprettTur.js
@@ -1,13 +1,37 @@
-﻿function validerOgLagTur() {
+﻿
+function validerLikeStasjoner() {
+
+    const start = document.getElementById("startstasjonAdmin").value;
+    const slutt = document.getElementById("endestasjonAdmin").value;
+
+    if (start === slutt) {
+        $("#feilStartStasjonAdmin").html("Stasjonsnavnene kan ikke være like. ")
+        $("#feilEndeStasjonAdmin").html("Stasjonsnavnene kan ikke være like. ")
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+function validerOgLagTur() {
     const StartstasjonOK = validerStartStasjonAdmin($("#startstasjonAdmin").val());
     const EndestasjonOK = validerEndeStasjonAdmin($("#endestasjonAdmin").val());
+    const ikkeLikeStasjoner = validerLikeStasjoner();
     const DatoOK = validerDatoAdmin($("#datoAdmin").val());
     const TidOK = validerTidAdmin($("#tidAdmin").val());
     const BarnePrisOK = validerBarnePrisAdmin($("#barnePrisAdmin").val());
     const VoksenPrisOK = validerVoksenPrisAdmin($("#voksenPrisAdmin").val());
-    if (StartstasjonOK && EndestasjonOK && DatoOK && TidOK && BarnePrisOK && VoksenPrisOK) {
+    if (StartstasjonOK && EndestasjonOK && ikkeLikeStasjoner && DatoOK && TidOK && BarnePrisOK && VoksenPrisOK) {
         opprettTur();
     }
+}
+
+function testFunction() {
+    onChange();
+    validerStartStasjonAdmin($("#startstasjonAdmin").val());
+    validerEndeStasjonAdmin($("#endestasjonAdmin").val());
+    validerLikeStasjoner();
 }
 
 
